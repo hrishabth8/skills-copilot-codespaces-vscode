84da816149e372e6c3ba35915ed8107932eb2645
Hrishab T H <95047829+hrishabth8@users.noreply.github.com>
2025-04-14 19:48:09 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,8 @@
+// Create web server
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const Comment = require('./models/comment');
+
